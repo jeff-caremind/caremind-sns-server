5c0a5ff7eec8bd83e23c177f9bf0ce5e9b9dc3ba
JISUlicious <ljshong@hanmail.net>
2023-11-06 15:45:02 +0900

    feat: createComment method on controller

--- src/presentation/controller/feed/feed.controller.ts
+++ src/presentation/controller/feed/feed.controller.ts
@@ -1,4 +1,15 @@
-import { Controller, Get, Inject } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Inject,
+  Post,
+  Body,
+  Param,
+  Headers,
+} from '@nestjs/common';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { JwtService } from '@nestjs/jwt';
+
 import { IFeedService } from 'src/domain/service/feed/feed.service.interface';
 import { FEED_SERVICE } from 'src/domain/service/ioc';
 
@@ -6,10 +17,26 @@ import { FEED_SERVICE } from 'src/domain/service/ioc';
 export class FeedController {
   constructor(
     @Inject(FEED_SERVICE) private readonly feedService: IFeedService,
+    private readonly JwtService: JwtService,
   ) {}
 
   @Get()
   async getAll(): Promise<any> {
     return await this.feedService.getAll();
+  }
+
+  @Post('/:postId/comment')
+  async createComment(
+    @Body() body: any,
+    @Param() postId: number,
+    @Headers('authorization') token: string,
+  ): Promise<void> {
+    const decoded = this.verifyToken(token);
+    return await this.feedService.createComment();
+  }
+
+  verifyToken(token: string): { aud: number } {
+    const decoded = this.JwtService.verify(token);
+    return decoded;
   }
 }
