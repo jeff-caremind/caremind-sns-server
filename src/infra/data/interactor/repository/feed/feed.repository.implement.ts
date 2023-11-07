5d93a9fc67db02e79859f916e3cbbfb35d511440
JISUlicious <ljshong@hanmail.net>
2023-11-07 12:31:02 +0900

    feat: 불필요한 import 삭제

--- src/infra/data/interactor/repository/feed/feed.repository.implement.ts
+++ src/infra/data/interactor/repository/feed/feed.repository.implement.ts
@@ -1,20 +1,14 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { IFeedRepository } from 'src/domain/interactor/data/repository/feed.repository.interface';
-import {
-  FEED_TYPEORM_REPOSITORY,
-  USER_TYPEORM_REPOSITORY,
-} from 'src/infra/data/typeorm/repository/ioc';
+import { FEED_TYPEORM_REPOSITORY } from 'src/infra/data/typeorm/repository/ioc';
 import { Repository } from 'typeorm';
 import { FeedVo } from '../../../typeorm/vo/feed.vo';
-import { UserVo } from 'src/infra/data/typeorm/vo/user.vo';
 
 @Injectable()
 export class FeedRepositoryImpl implements IFeedRepository {
   constructor(
     @Inject(FEED_TYPEORM_REPOSITORY)
     private readonly feedTypeormRepository: Repository<FeedVo>,
-    @Inject(USER_TYPEORM_REPOSITORY)
-    private readonly userTypeormRepository: Repository<UserVo>,
   ) {}
 
   async findAll(): Promise<FeedVo[]> {
